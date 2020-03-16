52ac65e5de8011eb7076c56820db63ab5494a658
evgenkarlson <V1z9l7o3m5@mail.ru>
2020-03-17 02:35:17 +0300

    Subjects

---
+++ N_o_t_e_s_(Полезные_записи)/-.c
@@ -0,0 +1,50 @@
+int i; 		/*an int*/
+int *p; 	/*an int pointer (ptr to an int)*/
+int a[]; 	/*an array of ints*/
+int f(); 	/*a function returning an int*/
+int **pp; 	/*a pointer to an int pointer (ptr to a ptr to an int)*/
+int (*pa)[]; 	/* a pointer to an array of ints*/ /* указатель на массив целых */
+int (*pf)(); 	/*a pointer to a function returning an int*/
+int *ap[]; 	/*an array of int pointers (array of ptrs to ints)*/
+int aa[][]; 	/*an array of arrays of ints*/
+int *fp(); 	/*a function returning an int pointer*/
+int ***ppp; 	/*a pointer to a pointer to an int pointer*/
+int (**ppa)[]; 	/*a pointer to a pointer to an array of ints*/
+int (**ppf)(); 	/*a pointer to a pointer to a function returning an int*/
+int *(*pap)[]; 	/*a pointer to an array of int pointers*/
+int (*paa)[][];	/*a pointer to an array of arrays of ints*/
+int *(*pfp)(); 	/*a pointer to a function returning an int pointer*/
+int **app[]; 	/*an array of pointers to int pointers*/
+int (*apa[])[];	/*an array of pointers to arrays of ints*/
+int (*apf[])();	/*an array of pointers to functions returning an int*/
+int *aap[][]; 	/*an array of arrays of int pointers*/
+int aaa[][][]; 	/*an array of arrays of arrays of int*/
+int **fpp(); 	/*a function returning a pointer to an int pointer*/
+int (*fpa())[];	/*a function returning a pointer to an array of ints*/
+int (*fpf())();	/*a function returning a pointer to a function returning an int*/
+
+
+int i;		/* переменная типа int */
+int *p;		/* указатель на int (от ptr до int) */
+int a[];	/* массив целых чисел */
+int f();	/* функция, возвращающая int */
+int **pp;	/* указатель на указатель на int (ptr на ptr на int) */
+int (*pa)[];	/* указатель на массив целых */
+int (*pf)();	/* указатель на функцию, возвращающую int */
+int *ap[];	/* массив указателей на int (массив от ptrs до ints) */
+int aa[][];	/* массив массивов целых */
+int *fp();	/* функция, возвращающая указатель на int */
+int ***ppp;	/* указатель на указатель на указатель на int */
+int (**ppa)[];	/* указатель на указатель на массив целых чисел */
+int (**ppf)();	/* указатель на указатель на функцию, возвращающую int */
+int *(*pap)[];	/* указатель на массив указателей int */
+int (*paa)[][];	/* указатель на массив массивов целых чисел */
+int *(*pfp)();	/* указатель на функцию, возвращающую указатель int */
+int **app[];	/* массив указателей на указатели int */
+int (*apa[])[];	/* массив указателей на массивы целых чисел */
+int (*apf[])();	/* массив указателей на функции, возвращающие int */
+int *aap[][];	/* массив массивов указателей int */
+int aaa[][][];	/* массив массивов массивов int */
+int **fpp();	/* функция, возвращающая указатель на int-указатель */
+int (*fpa())[];	/* функция, возвращающая указатель на массив целых чисел */
+int (*fpf())();	/* функция, возвращающая указатель на функцию, возвращающую int */
