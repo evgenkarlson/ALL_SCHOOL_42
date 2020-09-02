783396db45d6ee945a3ae2de96afdb5b3e2f1571
evgenkarlson <37370582+evgenkarlson@users.noreply.github.com>
2020-09-02 15:01:00 +0300

    subjects

--- Notes_to_help/-.c
+++ Notes_to_help/-.c
@@ -1,37 +1,12 @@
-int i; 		/*an int*/
-int *p; 	/*an int pointer (ptr to an int)*/
-int a[]; 	/*an array of ints*/
-int f(); 	/*a function returning an int*/
-int **pp; 	/*a pointer to an int pointer (ptr to a ptr to an int)*/
-int (*pa)[]; 	/* a pointer to an array of ints*/ /* указатель на массив целых */
-int (*pf)(); 	/*a pointer to a function returning an int*/
-int *ap[]; 	/*an array of int pointers (array of ptrs to ints)*/
-int aa[][]; 	/*an array of arrays of ints*/
-int *fp(); 	/*a function returning an int pointer*/
-int ***ppp; 	/*a pointer to a pointer to an int pointer*/
-int (**ppa)[]; 	/*a pointer to a pointer to an array of ints*/
-int (**ppf)(); 	/*a pointer to a pointer to a function returning an int*/
-int *(*pap)[]; 	/*a pointer to an array of int pointers*/
-int (*paa)[][];	/*a pointer to an array of arrays of ints*/
-int *(*pfp)(); 	/*a pointer to a function returning an int pointer*/
-int **app[]; 	/*an array of pointers to int pointers*/
-int (*apa[])[];	/*an array of pointers to arrays of ints*/
-int (*apf[])();	/*an array of pointers to functions returning an int*/
-int *aap[][]; 	/*an array of arrays of int pointers*/
-int aaa[][][]; 	/*an array of arrays of arrays of int*/
-int **fpp(); 	/*a function returning a pointer to an int pointer*/
-int (*fpa())[];	/*a function returning a pointer to an array of ints*/
-int (*fpf())();	/*a function returning a pointer to a function returning an int*/
-
 
 int i;		/* переменная типа int */
-int *p;		/* указатель на int (от ptr до int) */
+int *p;		/* указатель на int */
 int a[];	/* массив целых чисел */
 int f();	/* функция, возвращающая int */
-int **pp;	/* указатель на указатель на int (ptr на ptr на int) */
+int **pp;	/* указатель на указатель на int */
 int (*pa)[];	/* указатель на массив целых */
 int (*pf)();	/* указатель на функцию, возвращающую int */
-int *ap[];	/* массив указателей на int (массив от ptrs до ints) */
+int *ap[];	/* массив указателей на int */
 int aa[][];	/* массив массивов целых */
 int *fp();	/* функция, возвращающая указатель на int */
 int ***ppp;	/* указатель на указатель на указатель на int */
